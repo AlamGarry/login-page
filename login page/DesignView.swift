31bbfaca0929fdeb1222cd6a2ca07331e453dc3a
AlamGarry <alqifariq@gmail.com>
2020-06-22 21:13:13 +0700

    second commit

---
+++ login page/DesignView.swift
@@ -0,0 +1,36 @@
+//
+//  DesignView.swift
+//  login page
+//
+//  Created by apap on 22/06/20.
+//  Copyright © 2020 apap. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableView: UIView {
+    @IBInspectable var shadowColor: UIColor = UIColor.clear {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+
+    @IBInspectable var shadowRadius: CGFloat = 0 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+
+    @IBInspectable var shadowOpacity: CGFloat = 0 {
+        didSet {
+            layer.shadowOpacity = Float(shadowOpacity)
+        }
+    }
+
+    @IBInspectable var shadowOffsetY: CGFloat = 0 {
+        didSet {
+            layer.shadowOffset.height = shadowOffsetY
+        }
+    }
+}
