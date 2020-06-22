10705d93ec019d42df44a23a320efef44a727286
AlamGarry <alqifariq@gmail.com>
2020-06-22 18:37:57 +0700

    Initial Commit

---
+++ login page/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  login page
+//
+//  Created by apap on 22/06/20.
+//  Copyright © 2020 apap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
